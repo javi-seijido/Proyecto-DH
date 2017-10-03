4e8fda23a9fa55562deec9819978eeb54c342d38
1908dmoreira <32307724+1908dmoreira@users.noreply.github.com>
2017-10-03 09:47:03 -0300

    Importacion Inicial
    parte 1

---
+++ Diego/php/main_menu.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Menu</title>
+    <meta name="viewport" content="width=device-width">
+    <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
+    <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/styles_log.css">
+    <link rel="stylesheet" href="../css/styles_menu.css">
+  </head>
+  <body>
+    <div class="div_titulo">
+        <h1 class="titulo">Menu</h1>
+    </div>
+
+    <nav class="header">
+  			<ul class="menu">
+  				<li><a  href="registro_personal.php"> ABM Usuarios</a></li>
+          <li><a href="#"> ABM Personal</a></li>
+          <li><a href="#"> ABM Alarmas</a></li>
+          <li><a href="#"> ABM Suministros</a></li>
+          <li><a href="faq_alt.html"> Prosedimientos frecuentes</a></li>
+  				<li><a href="">Consultas/Informes</a>
+  					<ul>
+  						<li ><a class="sub_a_nav" href="">Submenu1</a></li>
+  						<li><a class="sub_a_nav" href="">Submenu2</a></li>
+  						<li><a class="sub_a_nav" href="">Submenu3</a></li>
+  						<li><a class="sub_a_nav" href="">Submenu4</a></li>
+  					</ul>
+  				</li>
+  			</ul>
+  		</nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <!-- <nav class="main_nav">
+         <ul>
+           <li><a href="registro_personal.php"> ABM Users.</a></li>
+           <li><a href="#"> ABM Vehiculos</a></li>
+           <li><a href="#"> ABM Alarmas</a></li>
+           <li><a href="#"> Siniestros.</a></li>
+           <li><a href="#"> Historial de Siniestros.</a></li>
+         </ul>
+      </nav> -->
+
+  </body>
+</html>
