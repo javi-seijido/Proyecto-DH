36a5d90462afc4c0865932c763bbef07bca93678
elColorRojo <32307675+elColorRojo@users.noreply.github.com>
2017-11-04 18:13:30 -0300

    Add files via upload

---
+++ func.php
@@ -0,0 +1,77 @@
+<?php
+	function validarUsuario_create($data){
+		$errores = [];
+
+		if (trim($data['codigo']) == '') {
+			$errores['codigo'] = 'Ingresar codigo';
+		}
+
+		if (trim($data['pass']) == '') {
+			$errores['pass'] = 'ingresar contraseña';
+		}
+
+		if (trim($data['repass']) == '') {
+			$errores['repass'] = 'Verificar contraseña';
+		} elseif (trim($data['pass']) != trim($data['repass'])) {
+			$errores['repass'] = 'Las contraseñas no coinciden!';
+		}
+
+		if (trim($data['email']) == '') {
+			$errores['email'] = 'ingresar email!';
+		} elseif (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+			$errores['email'] = 'El email ES INVALIDO!';
+		}
+
+		if (trim($data['nivel']) == '') {
+			$errores['nivel'] = 'Definir nivel';
+		}
+
+		return $errores;
+	}
+
+	function crearUsuario_create($usuario){
+		$usuarioFinal = [
+			'codigo' => $usuario['codigo'],
+			'email' => $usuario['email'],
+			'password' => password_hash($usuario['pass'], PASSWORD_DEFAULT),
+			'act' => $usuario['act'],
+			'nivel' => $usuario['nivel'],
+		];
+
+	// 	echo "<pre>";
+ // 	 var_dump($usuarioFinal);
+ // 	 echo "</pre>";
+ // 	 exit;
+
+
+		return $usuarioFinal;
+	}
+
+
+	function guardarUsuario_create($usuario, $db){
+
+		//  echo "<pre>";
+		//  var_dump($usuario);
+		//  echo "</pre>";
+		//  exit;
+
+		$stmt = $db->prepare("INSERT INTO user (username,email,password,act,perfil_id) VALUES(:username,:email,:password,:act,:perfil_id)");
+
+		$stmt->bindValue(':username',$usuario['codigo'], PDO::PARAM_STR);
+		$stmt->bindValue(':email',$usuario['email'], PDO::PARAM_STR);
+		$stmt->bindValue(':password',$usuario['password'], PDO::PARAM_STR);
+		$stmt->bindValue(':act',$usuario['act'], PDO::PARAM_INT);
+		$stmt->bindValue(':perfil_id',$usuario['nivel'], PDO::PARAM_INT);
+
+		$stmt->execute();
+}
+	// 	$usuarioFinal = json_encode($usuario);
+	//
+	// 	file_put_contents('usuarios.json', $usuarioFinal . PHP_EOL, FILE_APPEND); //PHP_EOL = Salto de linea
+	// }
+	//
+	// function enviarAAltaOk(){
+	// 	header('location: altaOk.php'); exit;
+	// }
+
+?>
---
+++ login_usuarios.php
@@ -0,0 +1,185 @@
+<?php
+	require_once('control_session.php');
+	require_once('func.php');
+	require('conexion.php');
+
+	$nivel = [
+		'1'=> 'Administrador',
+		'2'=> 'Perfil 1',
+		'3'=> 'Perfil 2'
+	];
+
+	$codigo = '';
+	// $pass = '';
+	// $verpass = '';
+	$email = '';
+
+	if ($_POST) {
+
+		// Validación
+		$erroresFinales = validarUsuario_create($_POST);
+
+		$codigo = $_POST['codigo'];
+		// $pass = $_POST['pass'];
+		// $verpass = $_POST['pass'];
+		$email = $_POST['email'];
+
+
+		if (empty($erroresFinales)) {
+			// Creo Usuario en ARRAY, $usuarioAGuardar recibe el return de la función crear usuario, que es un array asociativo que armé como yo quería.
+			$usuarioAGuardar = crearUsuario_create($_POST);
+
+			// Guardo Usuario en JSON, recibe el array guardado en la variable de arriba
+			guardarUsuario_create($usuarioAGuardar, $db);
+		}
+
+		header('location: index.php'); exit;
+
+
+
+		//  echo "<pre>";
+		//  var_dump($usuarioAGuardar);
+		//  echo "</pre>";
+		//  exit;
+	}
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Alta de Usuarios</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+  <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
+  <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="../css/normalize.css">
+	<link href="../css/styles_user.css" rel="stylesheet">
+	<link href="../css/style_menu.css" rel="stylesheet">
+</head>
+<body>
+		<?php require_once ('menu_cabecera.php');?>
+
+				<div class="container">
+					<h1>Alta de Usuarios</h1>
+				</div>
+
+
+
+
+        <div class="container">
+
+                <form class="personal_border" method="post">
+
+                  <section class="datos_personales">
+
+                            <img class="foto_perfil" src="../images/sin_perfil2.png" alt="foto_perfil">
+
+														<input type="file" name="avatar" style="display:none;" id="botonFileReal">
+
+		                        <input class="input-file" type="button" value="Subir Foto" onclick="document.getElementById('botonFileReal').click();" style="">
+
+
+	                          <?php if (isset($erroresFinales['imagen'])): ?>
+	                          <span style="color: red;"><img class="error_icon" src="../images/icon_error.png"></span>
+	                          <span class="span_error"><?=$erroresFinales['imagen'];?></span>
+	                        <?php endif; ?>
+
+                  </section>
+
+                    <section class="datos_personales">
+                            <label class="label_usr" for="codigo">Código Usuario:</label>
+                            <input class="us_campo"type="text" name="codigo"  value="<?=$codigo;?>">
+														<?php if (isset($erroresFinales['codigo'])): ?>
+                      				<span style="color: red;"><img class="error_icon" src="../images/icon_error.png"></span>
+                      				<span class="span_error"><?=$erroresFinales['codigo'];?></span>
+                      			<?php endif; ?>
+
+                            <br><br><br>
+
+
+                            <label class="label_usr" for="nombre">Contraseña:</label>
+                            <input class="us_campo"type="password" name="pass">
+														<?php if (isset($erroresFinales['pass'])): ?>
+                      				<span style="color: red;"><img class="error_icon" src="../images/icon_error.png"></span>
+                      				<span class="span_error"><?=$erroresFinales['pass'];?></span>
+                      			<?php endif; ?>
+                            <br><br><br>
+
+														<label class="label_usr" for="nombre">Verificar Contraseña:</label>
+                            <input class="us_campo"type="password" name="repass">
+														<?php if (isset($erroresFinales['repass'])): ?>
+															<span style="color: red;"><img class="error_icon" src="../images/icon_error.png"></span>
+															<span class="span_error"><?=$erroresFinales['repass'];?></span>
+														<?php endif; ?>
+                            <br><br><br>
+
+
+
+                            <!-- <label class="label_usr" for="correo">email:</label> value="<?=$email;?>"
+                            <input class="us_campo"type="text" name="email" value="<?=$email;?>">
+														<?php if (isset($erroresFinales['email'])): ?>
+															<span style="color: red;"><img class="error_icon" src="../images/icon_error.png"></span>
+															<span class="span_error"><?=$erroresFinales['email'];?></span>
+														<?php endif; ?> -->
+
+														<label class="label_usr" for="email">Correo electrónico:</label>
+														<input class="us_campo" type="text" name="email" value="<?=$email?>">
+														<?php if (isset($erroresFinales['email'])): ?>
+															<span style="color: red;"><img class="error_icon" src="../images/icon_error.png"></span>
+															<span class="span_error"><?=$erroresFinales['email'];?></span>
+														<?php endif; ?><br><br>
+
+                            <br><br>
+
+                            <label class="label_nivel">Nivel:</label>
+														<select class="boton_nivel" name="nivel">
+															<option  value="">Definir Nivel</option>
+															<?php foreach ($nivel as $letra => $valor): ?>
+																<?php if (isset($_POST['nivel']) && $letra == $_POST['nivel']): ?>
+																	<option selected value="<?=$letra;?>"><?=$valor;?></option>
+																<?php else: ?>
+																	<option value="<?=$letra;?>"><?=$valor;?></option>
+																<?php endif; ?>
+															<?php endforeach; ?>
+														</select>
+														<?php if (isset($erroresFinales['nivel'])): ?>
+															<span style="color: red;"><img class="error_icon" src="../images/icon_error.png"></span>
+															<span class="span_error"><?=$erroresFinales['nivel'];?></span>
+														<?php endif; ?>
+
+														<label class="activo">Activo</label>
+														<input class="check" type="checkbox" name="act" checked value="1">
+
+
+
+
+														<br><br>
+
+                            <div class="botonera">
+
+                              <button class="input" type="submit" name="enviar">Crear</button>
+                              <button class="input" type="submit">Buscar</button>
+                              <button class="input" type="submit">Modificar</button>
+
+
+                            </div>
+                  </section>
+
+                </form>
+
+    </div>
+      <footer class="footer_reg">
+
+          <a href="main_menu.php">
+              <img class="volver_logo" src="../images/volver.png">
+
+          </a>
+
+      </footer>
+
+
+
+  </body>
+</html>
